bcd0ba25589b6beb11c6be7b287566da81b9365b
sabyasachi94sen <sabyasachi9600@gmail.com>
2022-10-21 13:04:41 +0530

    updated form ui

--- src/features/helpers/validations/studentValid.tsx
+++ src/features/helpers/validations/studentValid.tsx
@@ -8,7 +8,7 @@ export const studentValid=yup.object({
     email: yup.string().email("Please enter a valid email").required("Email is a required field"),
     phone_number: yup.string().required("Phone number is a required field"),
     country_of_residence:yup.string().required("Country of residence is a required field"),
-    city_of_permanent_residence:yup.string().required("City is required field"),
+    city_of_residence:yup.string().required("City is required field"),
     country_of_citizenship:yup.string().required("Country of citizenship is a required field"),
     account_manager:yup.string().required("Assigned Staff is a required field"),
     country_of_boarding_school:yup.string().required("Country of boarding school is a required field")
