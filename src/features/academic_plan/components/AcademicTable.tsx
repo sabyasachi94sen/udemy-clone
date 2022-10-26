afd3f950005cda690e379411c8ec8ae0cb337e3b
sabyasachi94sen <sabyasachi9600@gmail.com>
2022-10-26 10:43:02 +0530

    updated table

--- src/features/academic_plan/components/AcademicTable.tsx
+++ src/features/academic_plan/components/AcademicTable.tsx
@@ -17,10 +17,20 @@ import {
 
 import { formatDate } from "@/shared/utils";
 import { useStoreData } from "@/shared/stores/modal.store";
+import { FaWindowRestore } from "react-icons/fa";
+import { isGeneratorFunction } from "util/types";
 
 export function AcademicTable({ onView,AepListQuery,page }): JSX.Element {
 
-  console.log(AepListQuery)
+
+  const [zoomVal,setZoomVal]=useState(Math.round(window.devicePixelRatio*100))
+ 
+
+  window.onresize=function(){
+    console.log(Math.round(window.devicePixelRatio*100))
+    setZoomVal(Math.round(window.devicePixelRatio*100))
+  
+  }
 
   // const updateCompleteMutation = useUpdateComplete();
 
@@ -30,6 +40,32 @@ export function AcademicTable({ onView,AepListQuery,page }): JSX.Element {
   const columnHelper = createColumnHelper<Account>();
 
   const [storeAepData, setStoreAepData] = useState([]);
+
+  const getCountry=()=>{
+
+    if(zoomVal==113)
+    return <div className={`w-[250%] text-center`}>Country of residence</div>
+    else if(zoomVal==100)
+    return <div className={`w-[300%] text-center`}>Country of residence</div>
+    else if(zoomVal==138)
+    return <div className={`w-[185%] text-center`}>Country of residence</div>
+    else
+    return <div className={`w-[225%] text-center`}>Country of residence</div>
+  }
+
+
+  const getManager=()=>{
+   
+    if(zoomVal==113)
+    return <div className="w-[190%] text-center">Account Manager</div>
+    else if(zoomVal==100)
+    return <div className="w-[230%] text-center">Account Manager</div>
+    else if(zoomVal==138)
+    return <div className="w-[130%] text-center">Account Manager</div>
+    else
+    return <div className="w-[160%] text-center">Account Manager</div>
+
+  }
 
   // REF: https://github.com/TanStack/table/issues/4241
   // to prevent this we're using any here
@@ -58,12 +94,13 @@ export function AcademicTable({ onView,AepListQuery,page }): JSX.Element {
       }),
       columnHelper.accessor((row) => row?.student?.country_of_citizenship, {
         id: "country",
-        header: <div className="w-[235%] text-center">Country of residence</div>,
-        cell: (info) => <div className="w-[105%] text-center">{info.getValue()}</div>,
+        
+        header: getCountry(),
+        cell: (info) => <div className="text-center">{info.getValue()}</div>,
       }),
       columnHelper.accessor((row) => row.account_manager?.username, {
         id: "manager_name",
-        header: <div className="w-[170%] text-center">Account Manager</div>,
+        header: getManager(),
         cell: (info) => <div className="text-center">{info.getValue()}</div>,
       }),
 
@@ -81,7 +118,7 @@ export function AcademicTable({ onView,AepListQuery,page }): JSX.Element {
         ),
       }),
     ],
-    [],
+    [zoomVal],
   );
 
   return (
