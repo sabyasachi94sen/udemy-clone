987111422b9713295315d8250a2f633e8376e197
sabyasachi94sen <sabyasachi9600@gmail.com>
2022-10-28 12:48:17 +0530

    updated login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,13 +8,14 @@ import { LoginForm } from "@/features/auth";
 
 export default function LoginPage() {
   return (
-    <main className="bg-white-100 flex h-screen items-center justify-between bg-[#0EA5A9]">
+    <main className="bg-white-100 flex h-screen items-center justify-between bg-[#3ab0fb]">
     
       <div className="relative w-[50%] h-[100vh] bg-[#012060] flex items-center justify-center">
       <img src="/images/ESSAI.png" className="w-[20%] h-[80vh]"/>
       </div>
-
+       <div className="w-[50%] flex justify-center">
       <LoginForm />
+      </div>
 
      
 
--- src/pages/reset-password-otp.tsx
+++ src/pages/reset-password-otp.tsx
@@ -3,12 +3,14 @@ import { ResetPasswordOTPForm } from "@/features/auth";
 
 function ResetPasswordOTP() {
   return (
-    <main className="bg-white-100 flex h-screen items-center justify-between bg-[#0EA5A9]">
+    <main className="bg-white-100 flex h-screen items-center justify-between bg-[#3ab0fb]">
      
      <div className="relative w-[50%] h-[100vh] bg-[#012060] flex items-center justify-center">
         <img src="/images/ESSAI.png" className="w-[20%] h-[80vh]"/>
       </div>
+      <div className="w-[50%] flex justify-center">
       <ResetPasswordOTPForm />
+      </div>
       
     </main>
   );
